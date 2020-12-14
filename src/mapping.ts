cf627b4e2bffa77846a2979751d5c9cad5f81a48
mtsalenc <mtsalenc@gmail.com>
2020-12-14 15:58:01 -0300

    feat: add request submission handler

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,71 +1,113 @@
-import { log, BigInt } from "@graphprotocol/graph-ts";
-import { RequestSubmitted, TokenStatusChange } from "../generated/ArbitrableTokenList/ArbitrableTokenList";
-import { Request, Token } from "../generated/schema";
+import { Address, BigInt, ByteArray, Bytes, crypto } from "@graphprotocol/graph-ts";
+import { ArbitrableTokenList, RequestSubmitted, TokenStatusChange } from "../generated/ArbitrableTokenList/ArbitrableTokenList";
+import { IArbitrator } from "../generated/ArbitrableTokenList/IArbitrator";
+import { Request, Token, Registry, Round } from "../generated/schema";
 
-
+// Result
 const PENDING = 'Pending'
 const ACCEPTED = 'Accepted'
 const REJECTED = 'Rejected'
 
+// RequestType
 const REGISTRATION = 'Registration'
 const REMOVAL = 'Removal'
 
+// Status
+const ABSENT = "Absent"
+const REGISTERED = "Registered"
+const REGISTRATION_REQUESTED = "RegistrationRequested"
+const CLEARING_REQUESTED = "ClearingRequested"
+
+// Ruling
+const NONE = "None"
+const ACCEPT = "Accept"
+const REJECT = "Reject"
+
+let ZERO_ADDRESS =
+  Bytes.fromHexString("0x0000000000000000000000000000000000000000") as Bytes
+
+function concatByteArrays(a: ByteArray, b: ByteArray): ByteArray {
+  let out = new Uint8Array(a.length + b.length);
+  for (let i = 0; i < a.length; i++) out[i] = a[i];
+  for (let j = 0; j < b.length; j++) out[a.length + j] = b[j];
+  return out as ByteArray;
+}
+
 export function handleRequestSubmitted(event: RequestSubmitted): void {
-  let token = Token.load(event.params._tokenID.toHexString())
+  let token = Token.load(event.params._tokenID.toHexString());
+  let tcr = ArbitrableTokenList.bind(event.address);
+  let registry = Registry.load(event.address.toHexString());
+  let tokenInfo = tcr.getTokenInfo(event.params._tokenID);
+
   if (token == null) {
-    token = new Token(event.params._tokenID.toHexString())
-    token.numberOfRequests = BigInt.fromI32(1)
-  } else {
-    token.numberOfRequests = token.numberOfRequests.plus(BigInt.fromI32(1))
+    token = new Token(event.params._tokenID.toHexString());
+    registry.numberOfSubmissions++;
+
+    token.registry = registry.id
+    token.name = tokenInfo.value0;
+    token.ticker = tokenInfo.value1;
+    token.address = tokenInfo.value2;
+    token.symbolMultihash = tokenInfo.value3;
+    token.status = tokenInfo.value4 == 2
+      ? REGISTRATION_REQUESTED
+      : CLEARING_REQUESTED;
+
+    token.numberOfRequests = BigInt.fromI32(0);
   }
-  token.save()
 
-  let id = event.params._tokenID.toHexString()+'-'+token.numberOfRequests.minus(BigInt.fromI32(1)).toString()
-  let request = new Request(id)
+  token.numberOfRequests = token.numberOfRequests.plus(BigInt.fromI32(1));
 
-  request.submissionTime = event.block.timestamp
+  let request = new Request(
+    token.id + '-0'
+  )
+  request.token = token.id
+  request.submissionTime = event.block.timestamp;
   request.result = PENDING
-  request.type = event.params._registrationRequest
+  request.type = token.status == REGISTRATION_REQUESTED
     ? REGISTRATION
-    : REMOVAL
-  request.requester = event.transaction.from
+    : REMOVAL;
+  request.requester = event.transaction.from;
+  request.resolutionTime = BigInt.fromI32(0);
+  request.disputed = false;
+  request.disputeID = BigInt.fromI32(0);
+  request.challenger = ZERO_ADDRESS
+  request.disputeOutcome = NONE;
+  request.numberOfRounds = BigInt.fromI32(1);
+  request.arbitrator = tcr.arbitrator();
+  request.arbitratorExtraData = tcr.arbitratorExtraData();
+  request.evidenceGroupID = BigInt.fromUnsignedBytes(
+    crypto.keccak256(
+      concatByteArrays(
+        event.params._tokenID,
+        ByteArray.fromI32(1)
+      )
+    ) as Bytes
+  );
 
-  request.resolutionTime = BigInt.fromI32(0)
+  request.metaEvidenceURI = request.type == REGISTRATION
+    ? registry.registrationMetaEvidenceURI
+    : registry.registrationMetaEvidenceURI
+
+  let round = new Round(
+    request.id + '-0'
+  )
+  round.request = request.id;
+  let arbitrator = IArbitrator.bind(request.arbitrator as Address);
+  let arbitrationCost = arbitrator.arbitrationCost(request.arbitratorExtraData);
+  round.amountPaidRequester = arbitrationCost.plus(
+    arbitrationCost.times(tcr.sharedStakeMultiplier()).div(tcr.MULTIPLIER_DIVISOR())
+  );
+  round.amountPaidChallenger = BigInt.fromI32(0)
+  round.hasPaidRequester = true;
+  round.hasPaidChallenger = false;
+  round.feeRewards = round.amountPaidRequester;
+
+  round.save()
   request.save()
+  token.save()
+  registry.save()
 }
 
 export function handleTokenStatusChange(event: TokenStatusChange): void {
-  if (event.params._status !== 0 && event.params._status !== 1) {
-    return // Request not yet resolved. Noop.
-  }
 
-  let token = Token.load(event.params._tokenID.toHexString())
-  if (token == null) {
-    log.error('T2CR: token {} not found. Bailing handleTokenStatusChange.',[event.params._tokenID.toHexString()])
-    return
-  }
-  let id = event.params._tokenID.toHexString()+'-'+token.numberOfRequests.minus(BigInt.fromI32(1)).toString()
-
-  let request = Request.load(id)
-  if (request == null) {
-    log.error('T2CR: Request {} not found. Bailing handleTokenStatusChange.',[id])
-    return
-  }
-
-  request.resolutionTime = event.block.timestamp;
-  if (request.type === REGISTRATION) {
-    if (event.params._status === 0) {
-      request.result = REJECTED
-    } else {
-      request.result = ACCEPTED
-    }
-  } else {
-    if (event.params._status === 0) {
-      request.result = ACCEPTED
-    } else {
-      request.result = REJECTED
-    }
-  }
-
-  request.save()
 }
