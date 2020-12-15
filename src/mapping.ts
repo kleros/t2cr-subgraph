a6ce8c2e1f30a12a79cd76cf8e3791f7ccb9ddad
mtsalenc <mtsalenc@gmail.com>
2020-12-15 16:19:10 -0300

    feat: handle rulings, evidence, challenge, appeals, execution

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,7 @@
 import { Address, BigInt, ByteArray, Bytes, crypto } from "@graphprotocol/graph-ts";
-import { ArbitrableTokenList, RequestSubmitted, TokenStatusChange } from "../generated/ArbitrableTokenList/ArbitrableTokenList";
+import { ArbitrableTokenList, FundAppealCall, RequestSubmitted, Ruling, SubmitEvidenceCall, TokenStatusChange } from "../generated/ArbitrableTokenList/ArbitrableTokenList";
 import { IArbitrator } from "../generated/ArbitrableTokenList/IArbitrator";
-import { Request, Token, Registry, Round } from "../generated/schema";
+import { Request, Token, Registry, Round, Evidence } from "../generated/schema";
 
 // Result
 const PENDING = 'Pending'
@@ -75,6 +75,7 @@ export function handleRequestSubmitted(event: RequestSubmitted): void {
   request.numberOfRounds = BigInt.fromI32(1);
   request.arbitrator = tcr.arbitrator();
   request.arbitratorExtraData = tcr.arbitratorExtraData();
+  request.numberOfEvidences = BigInt.fromI32(0);
   request.evidenceGroupID = BigInt.fromUnsignedBytes(
     crypto.keccak256(
       concatByteArrays(
@@ -109,5 +110,121 @@ export function handleRequestSubmitted(event: RequestSubmitted): void {
 }
 
 export function handleTokenStatusChange(event: TokenStatusChange): void {
+  let tcr = ArbitrableTokenList.bind(event.address);
+  let token = Token.load(event.params._tokenID.toHexString())
+  let request = Request.load(`${token.id}-${token.numberOfRequests.minus(BigInt.fromI32(1))}`)
+  if (event.params._challenger == ZERO_ADDRESS) {
+    // If there is no challenger, either:
+    // - This is a new request;
+    // - A request was executed (i.e. it was accepted).
 
+    if (token.status == REGISTRATION_REQUESTED || token.status == CLEARING_REQUESTED) {
+      return // New requests are handled in handleRequestSubmitted.
+    }
+
+    // Request executed.
+    request.resolutionTime = event.block.timestamp
+    token.status = request.type == REGISTRATION_REQUESTED
+      ? token.status = REGISTERED
+      : token.status = ABSENT;
+
+    token.save();
+    request.save();
+    return
+  }
+
+  // Other cases where the event is emmited are:
+  // - Dispute created;
+  // - Dispute appealed;
+  // - Ruling enforced.
+
+  // Ruling enforcement is handled in handleRuling, so we'll skip it here.
+  if (request.resolutionTime.gt(BigInt.fromI32(0))) return;
+
+  let round = Round.load(`${request.id}-${request.numberOfRounds.minus(BigInt.fromI32(1))}`)
+
+  let newRound = new Round(`${request.id}-${request.numberOfRounds}`)
+  newRound.request = request.id
+  newRound.amountPaidRequester = BigInt.fromI32(0)
+  newRound.amountPaidChallenger = BigInt.fromI32(0)
+  newRound.feeRewards = BigInt.fromI32(0)
+  newRound.hasPaidRequester = false;
+  newRound.hasPaidChallenger = false;
+
+  request.numberOfRounds = request.numberOfRounds.plus(BigInt.fromI32(1));
+
+  let arbitrator = IArbitrator.bind(request.arbitrator as Address);
+
+  if (!event.params._appealed) {
+    // Request was challenged (i.e. dispute created).
+    let arbitrationCost = arbitrator.arbitrationCost(request.arbitratorExtraData);
+    round.amountPaidChallenger = arbitrationCost.plus(
+      arbitrationCost.times(tcr.sharedStakeMultiplier()).div(tcr.MULTIPLIER_DIVISOR())
+    );
+    round.feeRewards = round.feeRewards.plus(
+      arbitrationCost.times(tcr.sharedStakeMultiplier()).div(tcr.MULTIPLIER_DIVISOR())
+    )
+
+  } else {
+    // Dispute appealed.
+    let appealCost = arbitrator.appealCost(request.disputeID, request.arbitratorExtraData);
+    round.feeRewards = round.feeRewards.minus(appealCost)
+  }
+
+  request.save()
+  round.save()
+  newRound.save()
+  token.save()
+}
+
+export function handleRuling(event: Ruling): void {
+  let tcr = ArbitrableTokenList.bind(event.address);
+  let tokenID = tcr.arbitratorDisputeIDToTokenID(event.params._arbitrator, event.params._disputeID);
+  let token = Token.load(tokenID.toHexString());
+  let request = Request.load(`${token.id}-${token.numberOfRequests.minus(BigInt.fromI32(1))}`)
+
+  let winner = event.params._ruling;
+   // Update token state
+   if (winner.equals(BigInt.fromI32(1))) { // Execute Request
+      if (token.status == REGISTRATION_REQUESTED)
+          token.status = REGISTERED;
+      else
+          token.status = ABSENT;
+  } else { // Revert to previous state.
+      if (token.status == REGISTRATION_REQUESTED)
+          token.status = ABSENT;
+      else
+          token.status = REGISTERED;
+  }
+
+  request.resolutionTime = event.block.timestamp;
+  request.result = winner.equals(BigInt.fromI32(0))
+    ? NONE
+    : winner.equals(BigInt.fromI32(1))
+    ? ACCEPT
+    : REJECT;
+
+  request.save();
+  token.save();
+}
+
+export function handleFundAppeal(call: FundAppealCall): void {
+
+}
+
+export function handleSubmitEvidence(call: SubmitEvidenceCall): void {
+  let token = Token.load(call.inputs._tokenID.toHexString())
+  let request = Request.load(
+    `${token.id}-${token.numberOfRequests.minus(BigInt.fromI32(1))}`
+  )
+
+  let evidence = new Evidence(
+    `${request.id}-${request.numberOfEvidences}`
+  )
+  evidence.submissionTime = call.block.timestamp;
+  evidence.submitter = call.from;
+  evidence.evidenceURI = call.inputs._evidence;
+  evidence.request = request.id;
+
+  request.numberOfEvidences = request.numberOfEvidences.plus(BigInt.fromI32(1));
 }
